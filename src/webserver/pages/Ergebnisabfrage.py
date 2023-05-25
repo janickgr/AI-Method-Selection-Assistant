bdddc83cc9212241c795edf2e56c1796f20cd367
Janick Greinacher <jgreina@stud.hs-heilbronn.de>
2023-05-25 18:06:07 +0200

    input all

--- src/webserver/pages/Ergebnisabfrage.py
+++ src/webserver/pages/Ergebnisabfrage.py
@@ -15,11 +15,23 @@ utils.display_session_state()
 
 # --------------------------------------------------------------------------------
 
-st.header('TODO....')
+st.header('Results')
 
+st.subheader('Company data input:')
 
-df = pd.DataFrame({'data': st.session_state['company_info_data'],
-                   'feature_input': st.session_state['feature_input_data']}
-                  )
+with st.expander('Inspect your entered company data input:'):
+    df_company_data = pd.DataFrame(
+        st.session_state['company_info_data'], index=['0'])
 
-st.write(df)
+    edited_df = st.experimental_data_editor(df_company_data)
+
+st.subheader('Data input:')
+
+with st.expander('Inspect your entered input data:'):
+    df_input_data = pd.DataFrame(
+        st.session_state['feature_input_data'], index=['0'])
+
+    edited_df = st.experimental_data_editor(df_input_data)
+
+if st.button('calculate methdod', key='schwanz'):
+    st.info('Pantello ist ein Hurensohn')
